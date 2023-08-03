5f96cf42036a38a0833911a74a07bf1e51654fb1
marco bernal <bernal.marco.a.84@gmail.com>
2023-08-03 10:14:39 -0600

    SC-1 - Test

--- src/main/java/com/mab/core/solid/open_closed/bad/ClaimApprovalManager.java
+++ src/main/java/com/mab/core/solid/open_closed/bad/ClaimApprovalManager.java
@@ -1,18 +1,21 @@
 package com.mab.core.solid.open_closed.bad;
 
+import lombok.extern.slf4j.Slf4j;
+
 //TODO: Fix all sonarlint issues
+@Slf4j
 public class ClaimApprovalManager {
 
     public void processHealthClaim(HealthInsuranceSurveyor surveyor) {
         if (surveyor.isValidClaim()) {
-            System.out.println("ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
+            log.info("ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
         }
     }
 
     //Updated method for adding support to vehicle claims
     public void processVehicleClaim(VehicleInsuranceSurveyor surveyor) {
         if (surveyor.isValidClaim()) {
-            System.out.println("ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
+            log.info("ClaimApprovalManager: Valid claim. Currently processing claim for approval....");
         }
     }
 }
