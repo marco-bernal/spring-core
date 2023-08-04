5848deef93d799e697a0a7cefe00c716ae7117f5
marco bernal <bernal.marco.a.84@gmail.com>
2023-08-04 09:47:11 -0600

    SC-1 - Test

--- src/main/java/com/mab/core/solid/dependency_inversion/bad/LightBulb.java
+++ src/main/java/com/mab/core/solid/dependency_inversion/bad/LightBulb.java
@@ -1,12 +1,15 @@
 package com.mab.core.solid.dependency_inversion.bad;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LightBulb {
 
     public void turnOn() {
-        System.out.println("LightBulb: Bulb turned on...");
+        log.info("LightBulb: Bulb turned on...");
     }
 
     public void turnOff() {
-        System.out.println("LightBulb: Bulb turned off...");
+        log.info("LightBulb: Bulb turned off...");
     }
 }
